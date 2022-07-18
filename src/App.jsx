45ef589e1dd974cfdf69be571c39ceafbfa8e8fb
Faith <fhgray96@gmail.com>
2022-07-17 19:50:20 -0500

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Joke from './Joke.jsx'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="joke">
+            <Joke 
+                setup="What did the fish say when he swam into a wall?" 
+                punchline="Dam"
+            />
+             <Joke 
+                setup="What do you call a fish with no eyes?" 
+                punchline="A fsh"
+            />
+            <Joke 
+                setup="What do you call a can opener that doesn’t work?" 
+                punchline="A can’t opener!"
+            />
+            <Joke 
+                setup="What do you get when you combine a rhetorical question and a joke?" 
+                punchline="Get it?"
+            />
     </div>
   )
 }
---
+++ src/Joke.jsx
@@ -0,0 +1,9 @@
+export default function Joke(props) {
+    
+    return (
+        <div className="joke">
+            <h2>{props.setup}</h2>
+            <h3>{props.punchline}</h3>
+        </div>
+    )
+}
